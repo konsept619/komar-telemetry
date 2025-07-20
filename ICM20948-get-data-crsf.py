c7110db2cb9ad8751ed617ee43c7f3a2a8063ab5
konsept619 <94557832+konsept619@users.noreply.github.com>
2025-07-20 22:34:10 +0200

    Create ICM20948-get-data-crsf.py
    Get data from IMU and send them to receiver by crsf protocol

---
+++ ICM20948-get-data-crsf.py
@@ -0,0 +1,67 @@
+from icm20948 import ICM20948
+import time
+import csv
+import serial
+from datetime import datetime
+import struct
+
+imu = ICM20948(i2c_addr=0x69)
+csv_file = 'imu_data.csv'
+ser = serial.Serial('/dev/ttyS0', 420000, timeout=1)
+
+headers = ['Accel_X_g', 'Accel_Y_g', 'Accel_Z_g', 'Gyro_X_dps', 'Gyro_Y_dps', 'Gyro_Z_dps',
+           'Mag_X_uT', 'Mag_Y_uT', 'Mag_Z_uT', 'Temp', 'Timestamp']
+
+def crsf_crc(payload):
+    crc = 0
+    for b in payload:
+        crc ^= b
+    return crc
+
+def send_imu_with_timestamp_as_crsf(timestamp_us, az):
+    payload_id = 0xF0
+    payload = struct.pack('<BIf', payload_id, timestamp_us, az)
+    frame = struct.pack('<BB', 0xC8, len(payload)) + payload
+    crc = crsf_crc(frame[2:])   
+    frame += bytes([crc])
+    ser.write(frame)
+
+try:
+    with open(csv_file, 'x', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(headers)
+except FileExistsError:
+    pass
+print(f"Saving data to {csv_file}")
+
+try:
+    while True:
+        ax, ay, az, gx, gy, gz = imu.read_accelerometer_gyro_data()
+        mx, my, mz = imu.read_magnetometer_data()
+        tmp = imu.read_temperature()
+        
+        timestamp_dt = datetime.now()
+        timestamp = timestamp_dt.strftime('%Y-%m-%d %H:%M:%S.%f')
+        timestamp_us = int(timestamp_dt.timestamp() * 1_000_000)
+
+        data_row = [ax, ay, az, gx, gy, gz, mx, my, mz, tmp]
+        data_row = [round(value, 5) for value in data_row]
+        data_row.append(timestamp)
+
+        with open(csv_file, 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(data_row)
+
+        
+        send_imu_with_timestamp_as_crsf(timestamp_us, float(az))
+
+        print(f"Accel: {ax:.2f}, {ay:.2f}, {az:.2f} g")
+        print(f"Gyro: {gx:.2f}, {gy:.2f}, {gz:.2f} dps")
+        print(f"Mag: {mx:.2f}, {my:.2f}, {mz:.2f} uT")
+        print(f"Temp: {tmp:.2f}")
+        print(f"Timestamp: {timestamp}, us={timestamp_us}")
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    ser.close()
+    print("\nStopped by user. Data saved to:", csv_file)
